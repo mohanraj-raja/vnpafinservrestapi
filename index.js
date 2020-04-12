09e3de1b9980c49b6a4e7e3c26ee7a59054677ef
mohanraj-raja <mohanraj.raja.tech@gmail.com>
2020-04-12 12:54:02 +0000

    add new url parser

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ app.post('/addnews',async function(req,res)
 {
   try
   {
-  console.log(req.body);
-  const client = await mongoClient.connect(url);
+  const client = await mongoClient.connect(url,{ useNewUrlParser: true });
   let news = req.body;
   news.postedOn=new Date();
+  console.log(req.body);
   let db = client.db("vnpsfinserv")
   let result = await db.collection('news').insertOne(news);
   res.send(result);}
@@ -33,17 +33,17 @@ app.post('/addnews',async function(req,res)
 
 });
 
-
-app.get('/simpleget', async function(req, res) {
-  console.log('login');
-  let client = await mongoClient.connect(url);
-  let db = client.db("NodeLearn");
-  let users = await db.collection("users").find({ "_id": req.body.email, "password": req.body.password });
-  if (users != undefined) {
-    res.send(1);
-  }
-  else {
-    res.send(-1);
+app.get('/getNews',async function(req,res)
+{
+try
+  {
+  const client = await mongoClient.connect(url,{ useNewUrlParser: true });
+  let db = client.db("vnpsfinserv")
+  let users = await db.collection("news").find({});
+  res.send(users);}
+  catch(err)
+  {
+    res.send(err)
   }
 });
 
@@ -52,40 +52,7 @@ app.get('/get',async function(req, res)
 res.send("test");
 });
 
-app.get('/getusers', async function(req, res) {
-  console.log('getusers');
-  let client = await mongoClient.connect(url);
-  let db = client.db("NodeLearn");
-  let users = await db.collection("users").find().toArray();
-  console.log(users);
-  client.close();
-  res.send(users);
-}
-);
 
-app.post('/createAccount', async function(req, res) {
-  console.log('createAccount');
-  let account = req.body;
-  console.log(req.body);
-  let client = await mongoClient.connect(url);
-  let db = client.db("NodeLearn");
-  await db.collection("users").insertOne(account);
-  client.close()
-  res.send(account)
-});
-
-app.get('/login', async function(req, res) {
-  console.log('login');
-  let client = await mongoClient.connect(url);
-  let db = client.db("NodeLearn");
-  let users = await db.collection("users").find({ "_id": req.body.email, "password": req.body.password });
-  if (users != undefined) {
-    res.send(1);
-  }
-  else {
-    res.send(-1);
-  }
-});
 
 app.listen(8082)
 console.log('hosted on 8082')
